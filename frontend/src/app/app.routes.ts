445fea1955b398bff106a23501bf3349d5aeff7c
Daniel Santiago <dsantiago@luno.com>
2024-10-18 18:38:44 +0200

    frontend: update animation direction and clean up

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -29,27 +29,6 @@ export const routes: Routes = [
 ];
 
 export const slideInAnimation = trigger('routeAnimations', [
-  transition('HomePage <=> SendPage', [
-    style({ position: 'relative' }),
-    query(':enter, :leave', [
-      style({
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        width: '100%',
-      }),
-    ]),
-    query(':enter', [style({ left: '-100%' })], { optional: true }),
-    query(':leave', animateChild(), { optional: true }),
-    group([
-      query(':leave', [animate('300ms ease-out', style({ left: '100%' }))], {
-        optional: true,
-      }),
-      query(':enter', [animate('300ms ease-out', style({ left: '0%' }))], {
-        optional: true,
-      }),
-    ]),
-  ]),
   transition('* <=> *', [
     style({ position: 'relative' }),
     query(
@@ -64,12 +43,12 @@ export const slideInAnimation = trigger('routeAnimations', [
       ],
       { optional: true },
     ),
-    query(':enter', [style({ left: '-100%' })], { optional: true }),
+    query(':enter', [style({ left: '100%' })], { optional: true }),
     query(':leave', animateChild(), { optional: true }),
     group([
       query(
         ':leave',
-        [animate('200ms ease-out', style({ left: '100%', opacity: 0 }))],
+        [animate('200ms ease-out', style({ left: '-100%', opacity: 0 }))],
         { optional: true },
       ),
       query(':enter', [animate('300ms ease-out', style({ left: '0%' }))], {
