042b60db5683e453cb5f7fad5894b8863b726ab3
Daniel Santiago <dsantiago@luno.com>
2024-10-18 18:04:52 +0200

    frontend: add submit button component

---
+++ frontend/src/app/submit-button/submit-button.component.ts
@@ -0,0 +1,27 @@
+import { Component, input, output } from '@angular/core';
+import {} from '@ng-icons/core';
+import { lucideLoader2, lucideArrowRight, lucideSend } from '@ng-icons/lucide';
+import { HlmIconComponent, provideIcons } from '@spartan-ng/ui-icon-helm';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+
+@Component({
+  selector: 'app-submit-button',
+  standalone: true,
+  imports: [HlmButtonDirective, HlmIconComponent],
+  providers: [
+    provideIcons({
+      lucideLoader2,
+      lucideArrowRight,
+      lucideSend,
+    }),
+  ],
+  templateUrl: './submit-button.component.html',
+  styleUrl: './submit-button.component.scss',
+})
+export class SubmitButtonComponent {
+  loading = input(false);
+
+  submitIcon = input<'lucideSend' | 'lucideArrowRight'>('lucideArrowRight');
+
+  clicked = output();
+}
