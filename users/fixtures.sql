889110b4b194757763ffe9a3bf1c4c41386b36d6
Ed <echarrod@users.noreply.github.com>
2024-10-18 16:45:06 +0100

    users: Update fixtures

--- users/fixtures.sql
+++ users/fixtures.sql
@@ -1,5 +1,5 @@
 INSERT INTO users (first_name, surname, country_code, phone_number, fiat_wallet_currency, usdc_wallet_address, image_url)
 VALUES
-    ('Ed', 'Harrod', 'UK', '+44 7911 123456', 'GBP', '0xA3bFa1234c8D4aFe897CefD0C843d89EdE4a90d1', 'https://gitlab.com/uploads/-/system/user/avatar/12203669/avatar.png'),
-    ('Daniel', 'Santiago', 'PP', '+680 555 6789', 'PHP', '0xB4eF21f6eD7b4832F8270De44dB7EeA234cd5678', 'https://gitlab.com/uploads/-/system/user/avatar/11505958/avatar.png'),
-    ('Solethu', 'Songca', 'SA', '+27 82 345 6789', 'ZAR', '0x9D5aA5eA2f13a2c478c8A489F4f523EdC9f12345', 'https://gitlab.com/uploads/-/system/user/avatar/20014891/avatar.png');
+    ('Ed', 'Harrod', 'GB', '+44 7911 123456', 'GBP', '0xA3bFa1234c8D4aFe897CefD0C843d89EdE4a90d1', 'https://gitlab.com/uploads/-/system/user/avatar/12203669/avatar.png'),
+    ('Daniel', 'Santiago', 'PT', '+680 555 6789', 'PHP', '0xB4eF21f6eD7b4832F8270De44dB7EeA234cd5678', 'https://gitlab.com/uploads/-/system/user/avatar/11505958/avatar.png'),
+    ('Solethu', 'Songca', 'ZA', '+27 82 345 6789', 'ZAR', '0x9D5aA5eA2f13a2c478c8A489F4f523EdC9f12345', 'https://gitlab.com/uploads/-/system/user/avatar/20014891/avatar.png');
