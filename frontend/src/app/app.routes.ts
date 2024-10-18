5a1d64be3a59819b90c9d547c60f0958a26afb36
Daniel Santiago <dsantiago@luno.com>
2024-10-18 10:56:53 +0200

    frontend: add routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./home/home.component').then((c) => c.HomeComponent),
+    data: { animation: 'HomePage' },
+  },
+  {
+    path: 'send',
+    loadComponent: () =>
+      import('./send/send.component').then((c) => c.SendComponent),
+    data: { animation: 'SendPage' },
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
