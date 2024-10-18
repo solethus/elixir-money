76d597e4b683353d1a4acdbd2d77fe1cc1357bc8
Ed <echarrod@users.noreply.github.com>
2024-10-18 09:48:34 +0100

    precommit: Only target staged files

---
+++ users/fixtures.go
@@ -0,0 +1,25 @@
+package users
+
+import (
+	"context"
+	_ "embed"
+	"log"
+
+	"encore.dev"
+	"encore.dev/storage/sqldb"
+	"encore.dev/storage/sqldb/sqlerr"
+)
+
+//go:embed fixtures.sql
+var fixtures string
+
+func init() {
+	if encore.Meta().Environment.Cloud == encore.CloudLocal {
+		if _, err := db.Exec(context.Background(), fixtures); err != nil {
+			// Fixtures might already be inserted
+			if sqldb.ErrCode(err) != sqlerr.UniqueViolation {
+				log.Fatalln("unable to add fixtures:", err)
+			}
+		}
+	}
+}
--- wallets/wallets.go
+++ wallets/wallets.go
@@ -3,8 +3,7 @@ package wallets
 // Service struct, learn more: https://encore.dev/docs/primitives/services-and-apis/service-structs
 //
 //encore:service
-type Service struct {
-}
+type Service struct{}
 
 // initService is automatically called by Encore when the service starts up.
 func initService() (*Service, error) {
