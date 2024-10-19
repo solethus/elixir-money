bab138bfa25634157b5efd5d13f5c215e5953eb2
Ed <echarrod@users.noreply.github.com>
2024-10-19 11:01:35 +0100

    users: Run fixtures on all envs, not just local:

--- users/fixtures.go
+++ users/fixtures.go
@@ -5,7 +5,6 @@ import (
 	_ "embed"
 	"log"
 
-	"encore.dev"
 	"encore.dev/storage/sqldb"
 	"encore.dev/storage/sqldb/sqlerr"
 )
@@ -14,13 +13,10 @@ import (
 var fixtures string
 
 func init() {
-	if encore.Meta().Environment.Cloud == encore.CloudLocal {
-		// TODO: Add unique constraint on DB, so we don't insert every time
-		if _, err := db.Exec(context.Background(), fixtures); err != nil {
-			// Fixtures might already be inserted
-			if sqldb.ErrCode(err) != sqlerr.UniqueViolation {
-				log.Fatalln("unable to add fixtures:", err)
-			}
+	if _, err := db.Exec(context.Background(), fixtures); err != nil {
+		// Fixtures might already be inserted
+		if sqldb.ErrCode(err) != sqlerr.UniqueViolation {
+			log.Fatalln("unable to add fixtures:", err)
 		}
 	}
 }
