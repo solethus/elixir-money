937c0a1680eea73b4315fcbddb3cb9b3c6ba3f50
Daniel Santiago <dsantiago@luno.com>
2024-10-20 16:25:38 +0200

    frontend: update mock user properties

--- frontend/src/app/state/user.service.ts
+++ frontend/src/app/state/user.service.ts
@@ -14,13 +14,14 @@ export class UserService {
       id: 1,
       first_name: 'Daniel',
       surname: 'Santiago',
-      phone_number: '123456789',
-      country_code: 'Portugal',
+      phone_number: '+6805556789',
+      country_code: 'PT',
       fiat_wallet_currency: 'EUR',
-      usdc_wallet_address: '0x123456789',
-      created_at: new Date().getTime().toString(),
-      updated_at: new Date().getTime().toString(),
-      image_url: 'https://randomuser.me/api/portraits',
+      usdc_wallet_address: '0xB4eF21f6eD7b4832F8270De44dB7EeA234cd5678',
+      created_at: new Date('2024-10-20 11:35:14.955098').toISOString(),
+      updated_at: new Date('2024-10-20 11:35:14.955098').toISOString(),
+      image_url:
+        'https://gitlab.com/uploads/-/system/user/avatar/11505958/avatar.png',
     });
     this.user = this.currentUser.asReadonly();
   }
